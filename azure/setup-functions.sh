bc02c7f5b93374a757ef459ea8a1dd96a8e1c470
jfc37 <joe.chapman3737@gmail.com>
2019-12-27 13:07:06 +1300

    Create setup-functions.sh

---
+++ azure/setup-functions.sh
@@ -0,0 +1,23 @@
+LOCALFUNCTIONPROJECT = functions-proj
+RESOURCEGROUP = functionsDemoGroup
+STORAGEACCOUNT = jfcfunctionsdemostorage
+FUNCTIONAPP = jfc-functions-demo
+
+# Initialise new local project for functions
+func init $LOCALFUNCTIONPROJECT
+
+# Create new local function
+func new --name MyHttpTrigger --template "HttpTrigger"
+
+# Create new group of functions in azure
+az group create --name $RESOURCEGROUP --location australiasoutheast
+az storage account create --name $STORAGEACCOUNT --location australiasoutheast --resource-group $RESOURCEGROUP --sku Standard_LRS
+az functionapp create --resource-group $RESOURCEGROUP --consumption-plan-location australiasoutheast \
+--name $FUNCTIONAPP --storage-account $STORAGEACCOUNT --runtime node
+
+# Deploy local functions to azure
+npm run build:production
+func azure functionapp publish $FUNCTIONAPP
+
+# delete
+az group delete --name $RESOURCEGROUP
